d14d712a117828cff60e0073be635d4eb3c1a8bc
DimitrisB <bdimitris@outlook.com>
2018-08-01 16:31:48 +0300

    start1

--- MyWordPad2/MainWindow.xaml.cs
+++ MyWordPad2/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace MyWordPad2
         {
             InitializeComponent();
         }
+
+
+
+        private void MouseEnterExitArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Exit the Application";
+        }
+
+        private void MouseLeaveArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Ready";
+        }
+
+        private void MouseEnterToolsHintsArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Show Spelling Suggestions";
+        }
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            // Close this window.
+            this.Close();
+        }
+
+        private void ToolsSpellingHints_Click(object sender, RoutedEventArgs e)
+        {
+            string spellingHints = string.Empty;
+            // Try to get a spelling error at the current caret location.
+            SpellingError error = txtData.GetSpellingError(txtData.CaretIndex);
+            if (error != null)
+            {
+                // Build a string of spelling suggestions.
+                foreach (string s in error.Suggestions)
+                {
+                    spellingHints += $"{s}\n";
+                }
+
+            // Show suggestions and expand the expander.
+                lblSpellingHints.Content = spellingHints;
+                expanderSpelling.IsExpanded = true;
+            }
+        }
     }
 }
